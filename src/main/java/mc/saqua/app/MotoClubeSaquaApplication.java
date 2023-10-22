1a7c4f4546c866673d91f0bed208b62e8e14b87a
Jhonattan Lima <jhonattanlima@yahoo.com.br>
2023-10-21 21:36:56 -0300

    Initial commit

---
+++ src/main/java/mc/saqua/app/MotoClubeSaquaApplication.java
@@ -0,0 +1,13 @@
+package mc.saqua.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoClubeSaquaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoClubeSaquaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mc/saqua/app/MotoClubeSaquaApplicationTests.java
@@ -0,0 +1,13 @@
+package mc.saqua.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoClubeSaquaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
